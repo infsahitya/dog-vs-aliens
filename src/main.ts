3500b00dc6190582d6d6be307d8cb852675cbbfd
infsahitya <sahitya.api@gmail.com>
2024-01-03 08:55:31 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-document.querySelector<HTMLDivElement>(
-  "#app",
-)!.innerHTML = `<h1>Dog v/s Aliens</h1>`;
+import "./styles/index.scss";
+import DOG_PLAYER_SPRITE from "./assets/sprites/shadow_dog.png";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+const canvasClass: Attr = document.createAttribute("id");
+
+canvasClass.value = "mainCanvas";
+canvas.setAttributeNode(canvasClass);
+
+const mainCanvasCTX: CanvasRenderingContext2D = canvas.getContext("2d")!;
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const dogPlayerImage: HTMLImageElement = new Image();
+dogPlayerImage.src = DOG_PLAYER_SPRITE;
+
+app.appendChild(canvas);
