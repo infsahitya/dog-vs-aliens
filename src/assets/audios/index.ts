7ddf9bc5336204c7abe34eed20a44e1aafdf5294
infsahitya <sahitya.api@gmail.com>
2024-01-09 09:25:48 +0530

    update

---
+++ src/assets/audios/index.ts
@@ -0,0 +1,3 @@
+import SoundEffect_Explosion from './8bit_bomb_explosion.wav';
+
+export { SoundEffect_Explosion };
---
+++ src/classes/__explosion.ts
@@ -0,0 +1,38 @@
+import { SoundEffect_Explosion } from "../assets/audios";
+import { EXPLOSION } from "../assets/sprites";
+
+interface ExplosionProps {
+  updateExplosion: () => void;
+  drawExplosion: () => void;
+}
+
+class __explosion implements ExplosionProps {
+  private sx: number = 0;
+  private readonly sy: number = 0;
+  private sw: number;
+  private sh: number;
+  private dy: number;
+  private dx: number;
+  private dw: number;
+  private dh: number;
+  private readonly spriteWidth: number = 200;
+  private readonly spriteHeight: number = 179;
+  private readonly staggerFrame: number = 5;
+  private readonly explosionAudio: HTMLAudioElement = new Audio();
+  private readonly explosionSprite: HTMLImageElement = new Image();
+
+  constructor(posX: number, posY: number) {
+    this.dx = posX;
+    this.dy = posY;
+    this.sw = this.spriteWidth;
+    this.sh = this.spriteHeight;
+    this.dw = this.spriteWidth / 2;
+    this.dh = this.spriteHeight / 2;
+    this.explosionSprite.src = EXPLOSION;
+    this.explosionAudio.src = SoundEffect_Explosion;
+  }
+  
+  updateExplosion() { }
+  
+  drawExplosion() { }
+}
