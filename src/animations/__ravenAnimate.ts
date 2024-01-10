29f95d5b9764f0ee8078ba5f75708ee90ed641a1
infsahitya <sahitya.api@gmail.com>
2024-01-10 15:26:05 +0530

    update

--- src/animations/__ravenAnimate.ts
+++ src/animations/__ravenAnimate.ts
@@ -1,14 +1,21 @@
 import { Raven } from "../classes";
 import { ravenCanvas } from "../components/canvas";
 
+const { CTX } = ravenCanvas;
+
 let lastRavenTime: number = 0;
 let nextRavenTime: number = 0;
 const ravenInterval: number = 500;
 
 let ravensCollection: Raven[] = [];
 
+window.addEventListener("click", (e) => {
+  const pixelColor = CTX.getImageData(e.x, e.y, 1, 1);
+  console.log(pixelColor);
+});
+
 function __ravenAnimate(timestamp: number) {
-  ravenCanvas.CTX.clearRect(0, 0, ravenCanvas.WIDTH, ravenCanvas.HEIGHT);
+  CTX.clearRect(0, 0, ravenCanvas.WIDTH, ravenCanvas.HEIGHT);
 
   const diffTime = timestamp - lastRavenTime;
   lastRavenTime = timestamp;
@@ -17,7 +24,13 @@ function __ravenAnimate(timestamp: number) {
   if (nextRavenTime > ravenInterval) {
     ravensCollection.push(new Raven());
     nextRavenTime = 0;
+
+    ravensCollection.sort((a, b) => {
+      return a.getDW() - b.getDW();
+    });
   }
+
+  console.log(ravensCollection);
 
   [...ravensCollection].forEach((raven) => {
     raven.updateRaven(diffTime);
--- src/classes/__raven.ts
+++ src/classes/__raven.ts
@@ -2,14 +2,16 @@ import { ENEMY_RAVEN } from "../assets/sprites";
 import { ravenCanvas } from "../components/canvas";
 
 interface RavenProps {
+  getDW: () => number;
+  getDH: () => number;
   updateRaven: (diffTime: number) => void;
   drawRaven: () => void;
 }
 
 class __raven implements RavenProps {
-  private readonly framesCount: number = 6;
   private readonly spriteWidth: number = 271;
   private readonly spriteHeight: number = 194;
+  private readonly framesCount: number = 6;
   private readonly ravenSpeed: number = Math.random() * 10 + 1;
   private readonly ravenSprite: HTMLImageElement = new Image();
   private readonly staggerFrame: number = Math.random() * 5 + 2;
@@ -24,6 +26,12 @@ class __raven implements RavenProps {
   private dx: number = ravenCanvas.WIDTH;
   private dy: number = Math.random() * (ravenCanvas.HEIGHT - this.spriteHeight);
   private animationFramesCount: number = 0;
+  private readonly ravenBoxColors: Record<"red" | "green" | "blue", number> = {
+    red: Math.floor(Math.random() * 255),
+    green: Math.floor(Math.random() * 255),
+    blue: Math.floor(Math.random() * 255),
+  };
+  private readonly ravenColor: string = `rgb(${this.ravenBoxColors.red}, ${this.ravenBoxColors.green}, ${this.ravenBoxColors.blue})`;
 
   private timeSinceLastFlap: number = 0;
   private flapInterval: number = 100;
@@ -33,6 +41,9 @@ class __raven implements RavenProps {
   constructor() {
     this.ravenSprite.src = ENEMY_RAVEN;
   }
+
+  getDW = () => this.dw;
+  getDH = () => this.dh;
 
   updateRaven(diffTime: number) {
     if (this.dy < 0 || this.dy > ravenCanvas.HEIGHT - this.spriteHeight / 3) {
@@ -57,7 +68,11 @@ class __raven implements RavenProps {
   }
 
   drawRaven() {
-    ravenCanvas.CTX.drawImage(
+    const { CTX } = ravenCanvas;
+
+    CTX.fillStyle = this.ravenColor;
+    CTX.fillRect(this.dx, this.dy, this.dw, this.dh);
+    CTX.drawImage(
       this.ravenSprite,
       this.currentSpritePosition * this.spriteWidth,
       0,
